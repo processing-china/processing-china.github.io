5cd19b0bd958f8d020d987c1d58c648399447174
Qianqianye <qianqian.ye44@gmail.com>
2019-01-04 16:23:56 -0800

    Chinese version for pcd Shanghai webpage

--- src/sketch.js
+++ src/sketch.js
@@ -1,50 +1,62 @@
-let body;
-let firstColor = {
-  h: 207,
-  s: 80,
-  b: 42
-};
-let secondColor = {
-  h: 210,
-  s: 80,
-  b: 60
-}
-let thirdColor = {
-  h: 272,
-  s: 70,
-  b: 35
-}
-let gradientDir = 30;
-let secondLocation = 35;
-let wordVariable;
-let firstColorString, secondColorString, thirdColorString;
+var num = 5;
+var mountains = [];
 
 function setup() {
-  noCanvas();
-  colorMode(HSB);
-  body = select('body');
-  setInterval(cycleText, 5000)
+	createCanvas(windowWidth, windowHeight);
+	shanShui();
 }
 
 function draw() {
-  firstColorString = `hsl(${firstColor.h},${firstColor.s}%,${firstColor.b}%)`;
-  secondColorString = `hsl(${secondColor.h},${secondColor.s}%,${secondColor.b}%)`;
-  thirdColorString = `hsl(${thirdColor.h},${thirdColor.s}%,${thirdColor.b}%)`;
-  let styleString = `linear-gradient(${gradientDir}deg, ${firstColorString} 0%, ${secondColorString} ${secondLocation}%, ${thirdColorString} 100%)`;
-  body.style('background', styleString);
-  firstColor.h = firstColor.h + random(-0.1, 0.1) % 360;
-  secondColor.h = secondColor.h + 0.01 % 360;
-  secondColor.h = secondColor.h + 0.01 % 360;
-  gradientDir = gradientDir + random(-0.1, 0.1) % 360;
-  //secondLocation = 40 + 20*sin(frameCount / 10000);
+	background(255);
+	noStroke();
+	ellipse(100, 100, 80, 80); //sun
+
+	for (var i = 0; i < mountains.length; i++) {
+		mountains[i].show();
+	}
 }
 
-function cycleText() {
-  const words = [" art, code and diversity ", " setup() and draw() ", " the joys of debugging ", " learning to program ", " createCanvas() ", " Processing and p5.js ", " art and the web ", " mousePressed() ", " ellipses and rects ", " software and visual literacy ", " coding and culture ", " open source culture ", " creative coding ", " generative design ", " community building ", " radical pedagogy ", " toolbuilding ", " crafting with code ", " game design ", " sound and code ", " interactivity ", " video pixels ", " data visualization ", " mouseX, mouseY "];
-  let choice = int(random(words.length));
-  let wordVar = select('#wordVariable');
-  if (wordVar){
-    //only run on first page (where wordVar exists)
-      wordVar.html(words[choice]);
-  }
+function shanShui() {
+	for (var i = 0; i < num; i++) {
+		mountains.push(new curveLine(i));
+	 }
+}
+
+function curveLine(_index) {
+	var index = _index;
+	var screen = 2000;
+	var screenHeight = 800;
+	var base = random (screen/4, screen/2); 
+	var start = random (-screen/2, screen/2); 
+
+	this.show = function (){
+			var ink = 20;
+
+			var c = color(0, 0, 0,ink);
+			fill(c);
+    		stroke(c);
+
+			var xoffset = map(mouseX, 0,screen, -100,100) * (index+1) ;
+
+				 for (var x = start; x < base + start; x++) {
+				 	var mapLoc = map(x, start, base+start, 0, 1);
+				 	var edgePercent = .2;
+
+				 	if(mapLoc< edgePercent){
+				 		stroke(0,0,0,(mapLoc*(1/edgePercent))*ink);
+				 	}else if(mapLoc> abs(1-edgePercent)){
+				 		var inverseLoc = abs(1-mapLoc);
+				 		stroke(0,0,0,(inverseLoc*1/edgePercent)*ink);
+				 	}
+
+					var nx = map(x, 0, screen, 0, 10);
+					var y = screenHeight * (noise(nx+index*10)*.7);
+					var xPos = x+ xoffset
+
+					line (xPos, y, xPos, y+(screenHeight-y)/2);
+					line (xPos, y, xPos, y+(screenHeight-y)/4);
+					line (xPos, y, xPos, y+(screenHeight-y)/8);
+					line (xPos, y, xPos, y+(screenHeight-y)/16);	
+				 }
+	}
 }
